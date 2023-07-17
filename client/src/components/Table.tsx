5b56ade9c002859f4d7fe1f82e47d04606b4e5d6
Kariy <ammr.arf@gmail.com>
2023-07-17 18:52:18 +0200

    table

--- client/src/components/Table.tsx
+++ client/src/components/Table.tsx
@@ -8,6 +8,7 @@ function Title() {
 			justify="space-evenly"
 			flex="1"
 			spacing="2rem"
+			className="my-4"
 		>
 			<Box className="flex gap-2 w-[115px]">
 				<div>Time</div> <div>(s)</div>
@@ -62,10 +63,26 @@ function Row({ time, height, speed, pitch, roll, fuel }: RowProps) {
 
 function Table() {
 	return (
-		<Card className="h-36 py-3 px-4" variant="pixelated" fontSize="sm">
-			<VStack align="normal">
+		<Card className="h-[480px]" variant="pixelated" fontSize="sm" overflow="auto">
+			<div className="sticky z-10 top-0 left-0 right-0 bg-[#202F20] px-5">
 				<Title />
 				<Divider />
+			</div>
+			<VStack align="normal" position="relative" className=" py-1 px-5">
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
+				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
 				<Row time="00:00" height="1000" speed="0" pitch="0" roll="0" fuel="100" />
 			</VStack>
 		</Card>
