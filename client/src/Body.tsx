340b8eb35f8b7651f851c6115f3d2c1ab0dbeda2
democritus <ponderingdemocritus@protonmail.com>
2023-07-17 21:36:46 +1000

    add networking

--- client/src/Body.tsx
+++ client/src/Body.tsx
@@ -1,11 +1,20 @@
 import Button from "./components/Button";
 import { Card, HStack } from "@chakra-ui/react";
 
+import {setupNetwork } from "./dojo/setupNetwork";
+
 function Body() {
+
+	function execute(){
+		setupNetwork().execute('start', []).catch((error) => {
+			console.log(error);
+		});
+	}
+
 	return (
 		<div className="h-16 max-w-[720px] flex-1">
 			{/* this is where u put all the main components */}
-			<Button>Something</Button>
+			<Button onClick={() => execute()}>Something</Button>
 			<Card className="h-32 " variant={"pixelated"}></Card>
 		</div>
 	);
